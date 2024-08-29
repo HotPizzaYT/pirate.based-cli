bb9108ce6023b7eb47c728b438ea08cc658b1901
HotPizzaYT <57576298+HotPizzaYT@users.noreply.github.com>
2024-08-28 20:55:02 -0500

    Add files via upload
    Fix issue with KTorrent not opening magnet

--- pirate-based/pirate_based/pirate_based.py
+++ pirate-based/pirate_based/pirate_based.py
@@ -5,6 +5,7 @@ import requests
 import json
 import os
 import platform
+import urllib.parse
 from datetime import datetime
 
 proxy = "none"
@@ -75,7 +76,7 @@ def query():
                     hash_value = details.get("info_hash", "N/A")
                     namefromt = details.get("name", "N/A")
 
-                    nameoftor = requests.utils.quote(namefromt)
+                    nameoftor = urllib.parse.quote(namefromt)
                     final = f"magnet:?xt=urn:btih:{hash_value}&dn={nameoftor}&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce"
 
                     print(f"Num of files: {num_files}\n")
@@ -90,6 +91,8 @@ def query():
                         if platform.system() == "Windows":
                             subprocess.Popen(["start", final], shell=True)
                         else:
+
+                            subprocess.Popen(["pkill", "ktorrent"])
                             subprocess.Popen(["xdg-open", final])
 
                         input("Press any key to continue browsing for torrents")
